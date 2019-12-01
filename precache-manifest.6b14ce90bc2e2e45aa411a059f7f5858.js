b7a9fc10d783d4281d4db361155c46ff111c1985
slowlife7 <parksang48@gmail.com>
2019-12-01 19:20:44 +0900

    Updates

---
+++ precache-manifest.6b14ce90bc2e2e45aa411a059f7f5858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ddd1a40b158b6c57dfb043272c0f0f0",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "468caf1cab3d3416f2e8",
+    "url": "/movie_app/static/css/main.642a403c.chunk.css"
+  },
+  {
+    "revision": "ce7cafe86a5907ed4230",
+    "url": "/movie_app/static/js/2.d8713903.chunk.js"
+  },
+  {
+    "revision": "468caf1cab3d3416f2e8",
+    "url": "/movie_app/static/js/main.9ae8f9ea.chunk.js"
+  },
+  {
+    "revision": "7259bafd2195d7f70cbd",
+    "url": "/movie_app/static/js/runtime-main.c454ae6d.js"
+  }
+]);
